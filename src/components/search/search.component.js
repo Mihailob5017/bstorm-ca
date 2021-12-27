3ebb33597d2d7e584561b74b387cdc99d8733c15
Mihailob5017 <mihailob5017@gmail.com>
2021-12-27 14:31:09 +0100

    added "Search Todos" functionality to the todo page

---
+++ src/components/search/search.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './search.style.css';
+
+const SearchComponent = ({ value, searchHandler }) => {
+	return (
+		<div className='search-component'>
+			<label htmlFor='search' className='search-label'>
+				Search Todos
+			</label>
+			<input
+				onChange={(e) => searchHandler(e.target.value)}
+				type='text'
+				value={value}
+				name='search'
+				className='search-input'
+				placeholder='Todo Title'
+			/>
+		</div>
+	);
+};
+
+export default SearchComponent;
--- src/pages/todo.page.js
+++ src/pages/todo.page.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './pages.style.css';
 
 // Components
 import Todo from '../components/todo/todo.component';
+import Search from '../components/search/search.component';
 
 // Redux
 import { useSelector } from 'react-redux';
@@ -10,15 +11,22 @@ import {} from '../redux/actions';
 
 // Code
 const TodoPage = () => {
-	const todos = useSelector((state) => state.todos);
+	const initialTodos = useSelector((state) => state.todos);
+	const [todos, setTodos] = useState(initialTodos);
+	const [searchValue, setSearchValue] = useState('');
+	const searchHandler = (value) => {
+		setSearchValue(value);
+		setTimeout(() => {
+			if (value === '') setTodos(initialTodos);
+			else setTodos(todos.filter((el) => el.title.includes(value)));
+		}, 500);
+	};
 
 	return (
 		<div className='todo-container'>
-			{/* Search */}
-
-			{/* Todo */}
 			{todos.length > 0 ? (
 				<>
+					<Search value={searchValue} searchHandler={searchHandler} />
 					<div className='todos-header'>
 						<h1 className='todo-num'>Num</h1>
 						<h1 className='todo-title'>Title</h1>
@@ -27,12 +35,12 @@ const TodoPage = () => {
 					{todos.map((todo, i) => (
 						<Todo key={i} num={i + 1} {...todo} />
 					))}
-					<button className='add-todo'>Add Todo</button>
 				</>
 			) : (
 				<h1 className='no-todos'>No TODO's left. Hooray!</h1>
 			)}
-			{/* Add Button */}
+
+			<button className='add-todo'>Add Todo</button>
 		</div>
 	);
 };
